815b55216e6139082a774b1620365a51a01976b7
Neal Lewis <neal@machinehead.retro>
2017-03-24 20:51:31 -0400

    clean up summaries

--- nlpfromscratch/summaries.py
+++ nlpfromscratch/summaries.py
@@ -5,14 +5,3 @@ def activation_summary(W):
   tf.summary.histogram(W.op.name + '/activations', W)
   tf.summary.scalar(W.op.name + '/sparsity', tf.nn.zero_fraction(W))
 
-def metrics(labels): 
-
-  with tf.variable_scope('metrics'): 
-
-    for metric in ['precision', 'recall', 'f1']:
-      with tf.variable_scope(metric): 
-        for label in labels: 
-          m = tf.get_variable(label, 1, initializer=tf.constant_initializer(0.0))
-          tf.add_to_collection('metrics', m)          
-
-
